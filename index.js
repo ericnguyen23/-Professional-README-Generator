a288ba44dbbc73c804ba30bc507aee134ecb518c
Eric <ericnguyen23@gmail.com>
2023-03-15 16:42:15 -0700

    Added var to store readme's formatted title

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const fs = require("fs");
 const inquirer = require("inquirer");
 
-console.log(inquirer);
+inquirer.prompt([{ name: "title", message: "README Title?" }]).then((data) => {
+  // remove "" marks and add #
+  let title = `# ${data.title.replace(/^""+/i, "")}`;
+
+  fs.writeFile("README.md", title, () => {
+    err ? console.log(err) : console.log("Success!");
+  });
+});
