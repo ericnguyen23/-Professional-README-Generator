56c12a431427e221ae648b33d3677706cd25af50
Eric <ericnguyen23@gmail.com>
2023-03-15 18:39:04 -0700

    Added readme formatting and capturing initial content

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# README Generator
+# sdf 
+## Description
+
+sdfd
+    
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-inquirer.prompt([{ name: "title", message: "README Title?" }]).then((data) => {
-  // remove "" marks and add #
-  let title = `# ${data.title.replace(/^""+/i, "")}`;
+inquirer
+  .prompt([
+    { name: "title", message: "Enter project title" },
+    { name: "desc", message: "Enter project description" },
+  ])
+  .then((data) => {
+    // remove "" marks and add #
+    let title = `# ${data.title.replace(/^""+/i, "")}`;
+    let desc = `\n${data.desc}`;
 
-  fs.writeFile("README.md", title, () => {
-    err ? console.log(err) : console.log("Success!");
+    let compiled = `${title} 
+## Description
+${desc}
+    `;
+
+    fs.writeFile("README.md", compiled, (err) => {
+      err ? console.log(err) : console.log("Success!");
+    });
+    // fs.appendFile("README.md", desc, (err) => {
+    //   err ? console.log(err) : console.log("Success!");
+    // });
   });
-});
